1e0a8ab3f9b5b0cfea5a1132fe69be2245583817
DESKTOP-P07N0T1\Mimi <yordan.detelinov@gmail.com>
2017-05-08 14:35:27 +0300

    last update

--- firstProject/routes/index.js
+++ firstProject/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express trtr' });
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: 'Hello, World!' });
 });
 
 module.exports = router;
